02feb7948bec65bcc51642a3f3c438e7a58017dc
Hortense <hortense@pelay.fr>
2020-03-05 17:03:48 +0100

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import PlayerAd from './components/PlayerAd';
+
+function App() {
+	return (
+		<div className="App">
+			<PlayerAd />
+		</div>
+	);
+}
+
+export default App;
---
+++ front/src/components/PlayerAd.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PlayerAd = (props) => {
+	return (
+		<div>
+			<h1>title</h1>
+			<p>game</p>
+			<p>date et heure</p>
+			<p>prix</p>
+			<p>annonce blablall fezkl hglhgrhgkehzjkflgjjrekgn kjer hger hgejr jkre gjkezh gknjkfd g;ekure </p>
+			<button>Répondre</button>
+		</div>
+	);
+};
+
+export default PlayerAd;
